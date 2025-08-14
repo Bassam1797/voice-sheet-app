8653fc7e808ba4e67898098621dc74a89608a110
Bassam1797 <bassam1797@gmail.com>
2025-08-14 22:48:50 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open('vs-cache-v1').then(cache=>cache.addAll([
+  e.waitUntil(caches.open('vs-cache-v2').then(cache=>cache.addAll([
     './','./index.html','./assets/style.css',
     './src/grid.js','./src/speech.js','./src/export.js','./src/storage.js','./src/utils.js',
     'https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js'
