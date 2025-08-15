ab068c2c264412fb6fceb2d2c24aa6ece011ee18
Bassam1797 <bassam1797@gmail.com>
2025-08-15 04:59:04 +0100

    Delete src/storage.js

--- src/storage.js
+++
@@ -1,1 +0,0 @@
-export const storage={get(key){try{return JSON.parse(localStorage.getItem(key));}catch{return null;}},set(key,val){localStorage.setItem(key,JSON.stringify(val));},remove(key){localStorage.removeItem(key);}};
